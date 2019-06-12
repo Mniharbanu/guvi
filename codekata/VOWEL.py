797316e9ff2558a660e0446e1097df4c1e720a6f
Mniharbanu <51689554+Mniharbanu@users.noreply.github.com>
2019-06-12 12:12:18 +0530

    Rename code kata/vowel.py to codekata/VOWEL.py

---
+++ codekata/VOWEL.py
@@ -0,0 +1,8 @@
+let=input()
+if(let=='*'or let=='%'or let=='#'or let=='$'or let=='^'):
+  print("invalid")
+elif(let=='a'or let=='e'or let=='i'or let=='o'or let=='u'):
+  print("Vowel")
+else:
+  print("Consonant")
+   
