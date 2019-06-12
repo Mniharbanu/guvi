2b043488be2537b2a7530d39eb17c37121f5644c
Mniharbanu <51689554+Mniharbanu@users.noreply.github.com>
2019-06-12 12:24:35 +0530

    Create alpha.py

---
+++ codekata/alpha.py
@@ -0,0 +1,6 @@
+con=input()
+con=con.lower()
+if(con=='a' or con=='b' or con=='c' or con=='d' or con=='e' or con=='f' or con=='g' or con=='h' or con=='i' or con=='j' or con=='k' or con=='l' or con=='m' or con=='n' or con=='o' or con=='p' or con=='q' or con=='r' or con=='s' or con=='t' or con=='u' or con=='v' or con=='w' or con=='x' or con=='y' or con=='z'):
+   print("Alphabet")
+else:
+  print("No")
