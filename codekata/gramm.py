95cb1bbf73ee4c53136f1873b2d4613e0d4ff574
Mniharbanu <51689554+Mniharbanu@users.noreply.github.com>
2019-06-21 15:03:29 +0530

    Create gramm.py

---
+++ codekata/gramm.py
@@ -0,0 +1,23 @@
+h1=input()
+if(h1=='I'):
+  print(1)
+elif(h1=='II'):
+  print(2)
+elif(h1=='III'):
+  print(3)
+elif(h1=='IV'):
+  print(4)
+elif(h1=='V'):
+  print(5)
+elif(h1=='VI'):
+  print(6)
+elif(h1=='VII'):
+  print(7)
+elif(h1=='VIII'):
+  print(8)
+elif(h1=='IX'):
+  print(9)
+elif(h1=='X'):
+  print(10)
+elif(h1=='XI'):
+  print(11)
