a73def831db97f3ab284ab722a8a903acfb77760
Mniharbanu <51689554+Mniharbanu@users.noreply.github.com>
2019-07-02 15:46:54 +0530

    Create holiday.py

---
+++ codekata/holiday.py
@@ -0,0 +1,12 @@
+a1 = 'Monday'
+b1 = 'Tuesday'
+c1 = 'Wednesday'
+d1= 'Thursday'
+e1 = 'Friday'
+f1 = 'Saturday'
+g1= 'Sunday'
+i = str(input())
+if(i==a1 or i==b1 or i==c1 or i==d1 or i==e1):
+  print("no")
+else:
+  print("yes")
